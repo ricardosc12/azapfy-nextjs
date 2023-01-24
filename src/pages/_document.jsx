ad56151cbf43aecec055c0f522a7a11a0d7eed4f
Ricardo <ricardo.spinola9@gmail.com>
2023-01-24 17:12:47 -0300

    fix title

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" className='h-full bg-gray-50'>
-      <Head />
+      <Head title='Azapfy - Velocidade para fazer'/>
       <body className='h-full'>
         <Main />
         <NextScript />
