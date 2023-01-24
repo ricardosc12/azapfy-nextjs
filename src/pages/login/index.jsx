daf8b3230d3d71fd67a753a9552714d70492a980
Ricardo <ricardo.spinola9@gmail.com>
2023-01-24 16:06:05 -0300

    fix route

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 import { LockClosedIcon } from '@heroicons/react/20/solid'
 
 export default function Login() {
-    const route = useRouter()
+    const router = useRouter()
     return (
         <>
         <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -29,7 +29,7 @@ export default function Login() {
                 </a>
                 </p>
             </div>
-            <form className="mt-8 space-y-6" action="/app/info" method="POST">
+            <div className="mt-8 space-y-6">
                 <input type="hidden" name="remember" defaultValue="true" />
                 <div className="-space-y-px rounded-md shadow-sm">
                 <div>
@@ -84,7 +84,7 @@ export default function Login() {
 
                 <div>
                 <button
-                    type='submit'
+                    onClick={()=>router.push("/app/info")}
                     className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
                     <span className="absolute inset-y-0 left-0 flex items-center pl-3">
@@ -93,7 +93,7 @@ export default function Login() {
                     Sign in
                 </button>
                 </div>
-            </form>
+            </div>
             </div>
         </div>
         </>
