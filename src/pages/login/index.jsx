1fca90fbf7556ec0fedc6b84c330bbf760f4e4b2
Douglasans <douglasans@hotmail.com>
2023-01-25 18:20:40 -0300

    ...

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -19,7 +19,7 @@ export default function Login() {
                 height={80}
                 />
                 <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                Sign with your account
+                Sign with your account - mudança douglas
                 </h2>
                 <p className="mt-2 text-center text-sm text-gray-600">
                 Or{' '}
