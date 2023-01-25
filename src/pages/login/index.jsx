ce5f6ae685ed3e927e36e8108aa73181efe1de91
Ricardo <ricardo.spinola9@gmail.com>
2023-01-25 12:14:44 -0300

    ...

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -19,7 +19,7 @@ export default function Login() {
                 height={80}
                 />
                 <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                Entrar na sua conta
+                Sign with your account
                 </h2>
                 <p className="mt-2 text-center text-sm text-gray-600">
                 Or{' '}
