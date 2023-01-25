de8cc2b454ad31c02280d3eca69662b0035ad473
Vitor Zillig <vitorzillig.azapfy@gmail.com>
2023-01-25 10:24:40 -0300

    changed title

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -19,7 +19,7 @@ export default function Login() {
                 height={80}
                 />
                 <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                Sign in to your account
+                Entrar na sua conta
                 </h2>
                 <p className="mt-2 text-center text-sm text-gray-600">
                 Or{' '}
