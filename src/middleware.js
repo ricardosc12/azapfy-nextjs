177c3c078dfd74c732b5c15bff1d4550a109509c
Ricardo <ricardo.spinola9@gmail.com>
2023-01-25 12:02:12 -0300

    meta

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
- 
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
 
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,7 +5,15 @@ import Head from 'next/head'
 export default function App({ Component, pageProps }) {  
 	return (
 		<>
-			<Head><title>Azapfy - Velocidade para fazer</title></Head>
+			<Head>
+				<title>Azapfy - Velocidade para fazer</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta name="theme-color" content="#000000" />
+				<meta
+				name="description"
+				content="Web site created using NextJS"
+				/>
+			</Head>
 			{Component.nav===true?
 			(
 				<Navbar><Component {...pageProps} /></Navbar>
