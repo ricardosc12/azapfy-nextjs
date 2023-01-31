342ea95f0b96862e2b7a6e8fafeb4a96f3cb629b
Ricardo <ricardo.spinola9@gmail.com>
2023-01-31 10:57:45 -0300

    redirecionamento de pagina

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -6,40 +6,20 @@ import { useRouter } from 'next/router'
 import Image from 'next/image'
 import Icon from "@pub/favicon.ico"
 import { signOut } from '@/api/Auth'
-
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
-
-const navigation = [
-    { name: 'Dashboard', href: '/app/dashboard', current: false },
-    { name: 'Information', href: '/app/info', current: true },
-    { name: 'Pesquisa', href: '/app/pesquisa', current: false },
-    { name: 'Store', href: '/app/store', current: false },
-  
-]
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '/login' },
-]
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+import { routes } from '@/routes'
 
 export default function Navbar({children}) {
 
     const router = useRouter()
 
-    const name = navigation.find(nav=>nav.href===router.pathname).name
-
     return (
         <main>
             <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+				<div>
+					{Object.entries(routes).map(([url,{title}])=>{
+						return <p><Link href={url}>{title}</Link></p>
+					})}
+				</div>
                 {children}
             </div>
         </main>
--- src/middleware.js
+++ src/middleware.js
@@ -2,12 +2,28 @@ import { NextResponse } from 'next/server'
 import { routes } from '@/routes'
 import CryptoJS from 'crypto-js'
 
+function checkModules(url,cookie){
+	try {
+		var modules = CryptoJS.AES.decrypt(cookie, "6c35aebeff1d62a35881af369772237b").toString(CryptoJS.enc.Utf8).split(',')
+		if(modules.includes(routes[url].name)) return true
+		return false
+	} catch {
+		return false
+	}
+}
+
 export async function middleware(request) {
 	let logged = request.cookies.get('_logged')?.value
-	let modules = CryptoJS.AES.decrypt(request.cookies.get('_module')?.value, "6c35aebeff1d62a35881af369772237b").toString(CryptoJS.enc.Utf8).split(',')
-	if(!routes.includes(request.nextUrl.pathname) || logged!=='1') {
+	let cookie_modules = request.cookies.get('_module')?.value
+	if(
+		!routes[request.nextUrl.pathname]
+		|| logged!=='1' 
+		|| (!cookie_modules && request.nextUrl.pathname!='/login')
+		|| !checkModules(request.nextUrl.pathname, cookie_modules)
+	)
+	{
 		const url = request.nextUrl.clone()
-		url.pathname = logged=='1'?'/app/dashboard':'/login'
+		url.pathname = (logged=='1'&&cookie_modules)?'/app/dashboard':'/login'
 		return NextResponse.redirect(url)
 	}
 	return NextResponse.next()
